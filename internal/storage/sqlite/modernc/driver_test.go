dad910d2cc3fe92083fc0fa76e8451f3803d6209
Omar Alani <omar.luqman@hey.com>
2026-08-16 18:07:16 -0500

    test(storage): consolidate sqlite driver conformance

--- internal/storage/sqlite/modernc/driver_test.go
+++ internal/storage/sqlite/modernc/driver_test.go
@@ -1,39 +1,17 @@
 package modernc_test
 
 import (
-	"database/sql"
-	"fmt"
-	"net/url"
 	"testing"
-	"time"
 
 	"github.com/omarluq/cord/internal/storage/conformance"
+	// Register the modernc sqlite database/sql driver for this conformance test binary.
 	_ "modernc.org/sqlite"
 )
 
 func TestDriverConformance(t *testing.T) {
 	t.Parallel()
-	conformance.Run(t, openDatabase)
-}
-
-func openDatabase(tb testing.TB, path string, timeout time.Duration) *sql.DB {
-	tb.Helper()
-
-	query := url.Values{}
-	query.Add("_pragma", fmt.Sprintf("busy_timeout(%d)", timeout.Milliseconds()))
-	query.Add("_pragma", "foreign_keys(1)")
-	query.Add("_pragma", "journal_mode(WAL)")
-
-	database, err := sql.Open("sqlite", "file:"+path+"?"+query.Encode())
-	if err != nil {
-		tb.Fatal(err)
-	}
-
-	tb.Cleanup(func() {
-		if err := database.Close(); err != nil {
-			tb.Errorf("close database: %v", err)
-		}
+	conformance.Run(t, conformance.Driver{
+		Name:       "sqlite",
+		DataSource: conformance.RepeatedPragmaDataSource,
 	})
-
-	return database
 }
