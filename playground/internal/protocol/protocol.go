cf59ce94a510a29cd00ebe5d5f48beac9bb0570c
Omar Alani <omar.luqman@hey.com>
2026-08-18 00:53:06 -0500

    refactor(playground): remove obsolete protocol media types

--- playground/internal/protocol/protocol.go
+++ playground/internal/protocol/protocol.go
@@ -1,15 +1,6 @@
 // Package protocol defines messages exchanged by the playground compiler and UI.
 package protocol
 
-const (
-	// ArtifactMediaType identifies a multipart compiler response containing graph metadata and WebAssembly.
-	ArtifactMediaType = "multipart/mixed"
-	// GraphMediaType identifies the workflow graph part of a compiler response.
-	GraphMediaType = "application/vnd.cord.graph+json"
-	// WASMMediaType identifies the WebAssembly part of a compiler response.
-	WASMMediaType = "application/wasm"
-)
-
 // Graph describes a workflow DAG extracted from submitted Go source.
 type Graph struct {
 	Nodes []Node `json:"nodes"`
